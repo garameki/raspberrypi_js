d71f24849aa51c55074a14684eba9635fcf8dd71
garameki <garameki@users.noreply.github.com>
2018-11-20 12:57:00 +0900

    Voice client for receiving 	renamed:    myVoice.js -> myClientVoice.js 	new file:   myConvertBase64ToVoice.js 	modified: myVoice.js 	create lib myConvertBase64ToVoice.js

--- myClientVoice.js
+++ myClientVoice.js
@@ -95,77 +95,8 @@
 		if(Math.floor(10000*(data.length%4)) != 0){
 			console.error("Data length mod 4 is not zero");
 		}else{
-			
-
-			var binaryArray = _convertBase64ToBinary(data);
-			var ab = binaryArray.buffer.slice();
-
-			var decodeco;
-			var nn = 2.0;
-			var context = new AudioContext();
-			var BUFFER_SIZE = context.sampleRate * nn;
-			var audioBuffer = context.createBuffer(1,BUFFER_SIZE,context.sampleRate);
-			var source = context.createBufferSource();
-
-			context.decodeAudioData(ab).then(function(decodedData) {
-				decodeco = decodedData;
-			},function(err){
-				console.error('Hey decode error in context.decodeAudioData()');
-				console.log(err);
-				console.log('ab4:',ab);
-			});
-
-			var hoge = setInterval(function(){
-				clearInterval(hoge);
-				console.log('PPPOOO',decodeco);
-				console.log('PPPOOO2',decodeco2);
-				var source = context.createBufferSource();
-				source.buffer = decodeco;
-				source.connect(context.destination);
-				source.start(0);
-			},100);
-
-
-
-
-
-
-
-		/* https://gist.github.com/borismus/1032746 */
-		function _convertBase64ToBinary(base64) {
-			var raw = window.atob(base64);
-			var rawLength = raw.length;
-			//var array = new Uint8Array(new ArrayBuffer(rawLength));
-			var array = new Uint8Array(new ArrayBuffer(rawLength));
-			//X	var array = new Float32Array(new ArrayBuffer(rawLength));
-			var array = new Int16Array(new ArrayBuffer(rawLength));
-
-			for(i = 0; i < rawLength; i+=1) {
-				array[i/2] = raw.charCodeAt(i)+raw.charCodeAt(i+1)*256;
-			}
-			return array;
-		};
-
-
-			//audioBuffer.getChannelData(0).set(data);
-			//	source = context.createMediaStreamSource(bytedata);
-
-
-
-			var bufferLoader = new BufferLoader(
-				context,
-				['/html/data/yoroshiku.mp3'],
-				finishloading
-			);
-			bufferLoader.load();
-
-			var decodeco2;
-			function finishloading(bufferList) {
-				decodeco2 = bufferList[0];
-			};
-
-
-		}/* else */
+			myConvertBase64ToVoice.say(data);
+		}
 	};
 })();
 
---
+++ myConvertBase64ToVoice.js
@@ -0,0 +1,80 @@
+(function(){
+
+	/*
+	 * Usage:
+	 *	myConvertBase64ToVoice.say(data);
+	 *
+	 *	Args:
+	 *		(Base64 decoded) data : this is decoded Base64 Strings
+	 *	Return:
+	 *		(AudioBuffer) return : AudioBuffer Object
+	 *
+	 */
+
+	/* global */
+	myConvertBase64ToVoice = { };
+
+	Object.defineProperty(myConvertBase64ToVoice,'say',{value:say,writable:false,enumerable:false,configurable:false});
+//	Object.defineProperty(myConvertBase64ToVoice,,'',{value:,writable:false,enumerable:false,configurable:false});
+
+
+	/* variables */
+
+	var decodeco;
+	var nn = 2.0;
+	var context = new AudioContext();
+	var BUFFER_SIZE = context.sampleRate * nn;
+	var audioBuffer = context.createBuffer(1,BUFFER_SIZE,context.sampleRate);
+	var source = context.createBufferSource();
+
+	/* methods */
+
+	function say(data){
+
+		try {
+			var binaryArray = _convertBase64ToBinary(data);
+			var ab = binaryArray.buffer.slice();/* arraybuffer is copied */
+		}catch(err){
+			console.error("Can't convert. augument must be decoded Base64 strings.");
+			console.error(err);
+			console.log('data=',data);
+		}
+
+		context.decodeAudioData(ab).then(function(decodedData) {
+			decodeco = decodedData;
+			var hoge = setInterval(function(){
+				clearInterval(hoge);
+				source.buffer = decodeco;
+				source.connect(context.destination);
+				source.start(0);
+			},100);
+
+		},function(err){
+			console.error('Decode error in AudioContext.decodeAudioData()');
+			console.error(err);
+			console.log(ab);
+		});
+
+	};
+
+	/* inner methods */
+
+	/* https://gist.github.com/borismus/1032746 + my tryal*/
+	/* HTTPRequestのものと比較して決定 */
+
+	function _convertBase64ToBinary(base64) {
+		var raw = window.atob(base64);
+		var rawLength = raw.length;
+		var array = new Int16Array(new ArrayBuffer(rawLength));
+
+		for(i = 0; i < rawLength; i+=1) {
+			array[i/2] = raw.charCodeAt(i) + raw.charCodeAt(i+1) * 256;
+		}
+		return array;
+	};
+
+
+})();
+
+
+
